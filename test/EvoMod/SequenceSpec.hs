cbfa2edf389928aa2930bd031b1dc669cde80df6
Dominik Schrempf <dominik.schrempf@gmail.com>
2019-01-18 09:45:38 +0100

    Start to work with hspec-discover.

---
+++ test/EvoMod/SequenceSpec.hs
@@ -0,0 +1,103 @@
+{- |
+Module      :  EvoMod.SequenceSpec
+Description :  Test EvolIO.
+Copyright   :  (c) Dominik Schrempf 2018
+License     :  GPL-3
+
+Maintainer  :  dominik.schrempf@gmail.com
+Stability   :  unstable
+Portability :  portable
+
+Creation date: Fri Oct  5 14:25:42 2018.
+
+-}
+
+
+module EvoMod.SequenceSpec
+  (spec) where
+
+import qualified Data.ByteString.Lazy.Char8         as B
+import           Data.Either
+import           Test.Hspec
+
+import           EvoMod.Data.Alphabet.Alphabet
+import           EvoMod.Data.Sequence.MultiSequenceAlignment
+import           EvoMod.Data.Sequence.Sequence
+import           EvoMod.Data.Sequence.Filter
+import           EvoMod.Import.Sequence.Fasta
+import           EvoMod.Export.Sequence.Fasta
+import           EvoMod.Tools
+
+dataDir :: String
+dataDir = "data/"
+
+fastaNucleotideFN :: String
+fastaNucleotideFN = dataDir ++ "Nucleotide.fasta"
+
+fastaNucleotideIUPACFN :: String
+fastaNucleotideIUPACFN = dataDir ++ "NucleotideIUPAC.fasta"
+
+fastaAminoAcidFN :: String
+fastaAminoAcidFN = dataDir ++ "AminoAcid.fasta"
+
+fastaErroneousFN :: String
+fastaErroneousFN = dataDir ++ "Erroneous.fasta"
+
+fastaDifferentLengthFN :: String
+fastaDifferentLengthFN = dataDir ++ "NucleotideDifferentLength.fasta"
+
+longestSequenceInFileBS :: B.ByteString
+longestSequenceInFileBS = B.unlines $ map B.pack [ ">SEQUENCE_3"
+                                                 , "ATTTAAAAAAACCCAAAACCCGGGCCCCGGGTTTTTTTA" ]
+
+longestSequenceInFile :: Sequence
+longestSequenceInFile = parseByteStringWith (fastaSequence $ alphabet DNA) longestSequenceInFileBS
+
+fastaDifferentLengthTrimmedFN :: String
+fastaDifferentLengthTrimmedFN = dataDir ++ "NucleotideDifferentLengthTrimmed.fasta"
+
+spec :: Spec
+spec = do
+  describe "Sequence.longest" $
+    it "finds the longest sequence"$ do
+    ss <- parseFileWith (fastaFile DNA) fastaDifferentLengthFN
+    longest ss `shouldBe` longestSequenceInFile
+
+  describe "Filter.filterLongerThan" $
+    it "filters sequences that are longer than a specified length" $ do
+    ss  <- parseFileWith (fastaFile DNA) fastaDifferentLengthFN
+    ss' <- parseFileWith (fastaFile DNA) fastaDifferentLengthTrimmedFN
+    filterLongerThan 10 ss `shouldBe` ss'
+
+  describe "Fasta.fastaMSANucleotide" $ do
+    it "parses a fasta file with nucleotide sequences with equal length" $ do
+      msa <- parseFileWith (fastaFileMSA DNA) fastaNucleotideFN
+      msaNSequences msa `shouldBe` 3
+      msaLength msa `shouldBe` 40
+
+    it "parses a fasta file with nucleotide IUPAC sequences with equal length" $ do
+      msa <- parseFileWith (fastaFileMSA DNA_IUPAC) fastaNucleotideIUPACFN
+      msaNSequences msa `shouldBe` 3
+      msaLength msa `shouldBe` 40
+
+    it "should not parse erroneous files" $ do
+      emsa <- runParserOnFile (fastaFile DNA) fastaErroneousFN
+      emsa  `shouldSatisfy` isLeft
+
+  describe "Fasta.fastaMSAAminoAcid" $ do
+    it "parses a fasta file with amino acid sequences with equal length" $ do
+      msa <- parseFileWith (fastaFileMSA Protein) fastaAminoAcidFN
+      msaNSequences msa `shouldBe` 2
+      msaLength msa `shouldBe` 237
+
+    it "should not parse erroneous files" $ do
+      msa <- runParserOnFile (fastaFile Protein) fastaErroneousFN
+      msa  `shouldSatisfy` isLeft
+
+  describe "Fasta.sequencesToFasta" $
+    it "should create a fasta bytestring that, when parsed again, is the original sequence" $ do
+      ss <- parseFileWith (fastaFile DNA_IUPAC) fastaNucleotideIUPACFN
+      let f   = sequencesToFasta ss
+          ss' = parseByteStringWith (fastaFile DNA_IUPAC) f
+      ss `shouldBe` ss'
+
