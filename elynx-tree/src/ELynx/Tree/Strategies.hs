1c79453c6f47b920dc86ea35608ef368c2c5a93c
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-09-07 23:40:06 +0200

    some minor changes

--- elynx-tree/src/ELynx/Tree/Strategies.hs
+++ elynx-tree/src/ELynx/Tree/Strategies.hs
@@ -21,9 +21,9 @@ import Control.Parallel.Strategies
 import Data.Foldable
 import ELynx.Tree.Rooted
 
--- | Parallel evaluation strategy.
+-- | Parallel evaluation strategy for a tree.
 --
--- Evaluate the tree in parallel up to a given layer.
+-- Evaluate the sub trees at given layer in parallel.
 parTree :: (NFData e, NFData a) => Int -> Strategy (Tree e a)
 parTree 0 t = rdeepseq t
 parTree n (Node br lb ts)
@@ -40,17 +40,11 @@ parTree n (Node br lb ts)
 branchFold :: (e -> e -> e) -> Tree e a -> e
 branchFold f (Node br _ ts) = foldl' f br $ map (branchFold f) ts
 
-myParList :: NFData a => Strategy [a]
-myParList [] = return []
-myParList ts = do
-  ts' <- parList rdeepseq $ tail ts
-  t' <- rdeepseq $ head ts
-  return $ t' : ts'
-
--- | Fold over branches up to given layer in parallel.
+-- | Fold over branches. Evaluate the given layer in parallel.
 parBranchFold :: NFData e => Int -> (e -> e -> e) -> Tree e a -> e
 parBranchFold 0 f t = branchFold f t
-parBranchFold 1 f (Node br _ ts) = foldl' f br (map (branchFold f) ts `using` myParList)
+parBranchFold 1 f (Node br _ ts) =
+  foldl' f br (map (branchFold f) ts `using` parList rdeepseq)
 parBranchFold n f (Node br _ ts)
   | n >= 2 = foldl' f br $ map (parBranchFold (n - 1) f) ts
   | otherwise = error "parBranchFold: n is zero or negative."
@@ -58,10 +52,11 @@ parBranchFold n f (Node br _ ts)
 branchFoldMap :: (e -> f) -> (f -> f -> f) -> Tree e a -> f
 branchFoldMap f g (Node br _ ts) = foldl' g (f br) $ map (branchFoldMap f g) ts
 
--- | Map and fold over branches up to given layer in parallel.
+-- | Map and fold over branches. Evaluate the given layer in parallel.
 parBranchFoldMap :: NFData f => Int -> (e -> f) -> (f -> f -> f) -> Tree e a -> f
 parBranchFoldMap 0 f g t = branchFoldMap f g t
-parBranchFoldMap 1 f g (Node br _ ts) = foldl' g (f br) (map (branchFoldMap f g) ts `using` myParList)
+parBranchFoldMap 1 f g (Node br _ ts) =
+  foldl' g (f br) (map (branchFoldMap f g) ts `using` parList rdeepseq)
 parBranchFoldMap n f g (Node br _ ts)
   | n >= 2 = foldl' g (f br) $ map (parBranchFoldMap (n - 1) f g) ts
   | otherwise = error "parBranchFoldMap: n is zero or negative."
