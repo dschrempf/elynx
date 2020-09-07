116783a017271eaea4ac0ec166a649df5332452d
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-09-07 15:21:09 +0200

    strategies work

--- elynx-tree/bench/Bench.hs
+++ elynx-tree/bench/Bench.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 -- |
 -- Module      :  Bench
 -- Description :  Various benchmarks
@@ -12,9 +14,12 @@
 module Main where
 
 import Criterion.Main
+import Data.Bifunctor
 import qualified Data.ByteString.Char8 as BS
-import ELynx.Tools
+import ELynx.Tools hiding (Random)
 import ELynx.Tree
+import ELynx.Tree.Simulate.PointProcess
+import System.Random.MWC
 
 treeFileMany :: FilePath
 treeFileMany = "data/Many.trees"
@@ -22,8 +27,35 @@ treeFileMany = "data/Many.trees"
 getManyTrees :: IO (Forest Phylo BS.ByteString)
 getManyTrees = parseFileWith (someNewick Standard) treeFileMany
 
+hugeTree :: IO (Tree Length Int)
+hugeTree = create >>= simulateReconstructedTree 1000 Random 1.0 0.9
+
+sinN :: Int -> Double -> Double
+sinN n x = iterate sin x !! n
+
+hugeTreeCalc :: Tree Length Int -> Tree Double Int
+hugeTreeCalc = first (sinN 200 . getLen)
+
+hugeTreeCalcPar :: Tree Length Int -> Tree Double Int
+hugeTreeCalcPar t = hugeTreeCalc t `using` parTree
+
+hugeTreeCalcPar2 :: Tree Length Int -> Tree Double Int
+hugeTreeCalcPar2 t = hugeTreeCalc t `using` parTree2
+
+hugeTreeCalcPar3 :: Tree Length Int -> Tree Double Int
+hugeTreeCalcPar3 t = hugeTreeCalc t `using` parTree3
+
 main :: IO ()
 main = do
-  ts <- getManyTrees
+  !ts <- getManyTrees
+  !ht <- hugeTree
   defaultMain
-    [bgroup "bipartition" [bench "manyTrees" $ nf (map bipartitions) ts]]
+    [ bgroup "bipartition" [bench "manyTrees" $ nf (map bipartitions) ts],
+      bgroup
+        "strategies"
+        [ bench "exp, sequential" $ nf hugeTreeCalc ht,
+          bench "exp, parallel 1" $ nf hugeTreeCalcPar ht,
+          bench "exp, parallel 2" $ nf hugeTreeCalcPar2 ht,
+          bench "exp, parallel 3" $ nf hugeTreeCalcPar3 ht
+        ]
+    ]
--- elynx-tree/src/ELynx/Tree/Strategies.hs
+++ elynx-tree/src/ELynx/Tree/Strategies.hs
@@ -10,8 +10,24 @@
 --
 -- Creation date: Mon Sep  7 13:36:45 2020.
 module ELynx.Tree.Strategies
-  (
+  ( parTree,
+    parTree2,
+    parTree3,
+    using,
   )
 where
 
 import Control.Parallel.Strategies
+import ELynx.Tree.Rooted
+
+-- | Evaluates all elements of the 'forest' in parallel.
+parTree :: (NFData e, NFData a) => Strategy (Tree e a)
+parTree (Node br lb ts) = Node <$> rdeepseq br <*> rdeepseq lb <*> parTraversable rdeepseq ts
+
+-- | Evaluates all elements of the 'forest's of the 'forest' in parallel.
+parTree2 :: (NFData e, NFData a) => Strategy (Tree e a)
+parTree2 (Node br lb ts) = Node <$> rdeepseq br <*> rdeepseq lb <*> parTraversable parTree ts
+
+-- | Evaluates all elements of the 'forest's of the 'forest's of the 'forest' in parallel.
+parTree3 :: (NFData e, NFData a) => Strategy (Tree e a)
+parTree3 (Node br lb ts) = Node <$> rdeepseq br <*> rdeepseq lb <*> parTraversable parTree2 ts
