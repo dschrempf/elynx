b7906a36da07ec51c263a841183ad035cb40d81d
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-02-17 19:12:27 +0100

    bench stub

---
+++ elynx-markov/bench/Bench.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      :  Bench
+-- Description :  Benchmark Markov process along tree
+-- Copyright   :  (c) 2021 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Wed Feb 17 17:42:30 2021.
+module Bench
+  (
+  )
+where
+
+import qualified ELynx.Simulate.MarkovProcessAlongTree as M
+
