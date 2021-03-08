fd5cb37edcaacda74094a4fb18357e3107114f75
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-03-08 15:10:50 +0100

    don't error when n is negative, be sequential

--- elynx-tree/src/ELynx/Tree/Parallel.hs
+++ elynx-tree/src/ELynx/Tree/Parallel.hs
@@ -32,31 +32,28 @@ myParList s xs = do
 -- Evaluate the sub trees up to given layer in parallel.
 parTree :: (NFData e, NFData a) => Int -> Strategy (Tree e a)
 parTree n t@(Node br lb ts)
-  | n == 0 = rdeepseq t
   | n == 1 = do
     ts' <- myParList rdeepseq ts
     return $ Node br lb ts'
   | n >= 2 = do
     ts' <- myParList (parTree (n -1)) ts
     return $ Node br lb ts'
-  | otherwise = error "parTree: n is negative."
+  | otherwise = rdeepseq t
 
 branchFoldMap :: (e -> f) -> (f -> f -> f) -> Tree e a -> f
 branchFoldMap f op (Node br _ ts) = foldl' op (f br) $ map (branchFoldMap f op) ts
 
 -- | Map and fold over branches. Evaluate the sub trees up to given layer in parallel.
 parBranchFoldMap :: NFData f => Int -> (e -> f) -> (f -> f -> f) -> Tree e a -> f
-parBranchFoldMap 0 f op t = branchFoldMap f op t
-parBranchFoldMap n f op (Node br _ ts)
+parBranchFoldMap n f op t@(Node br _ ts)
   | n >= 1 = foldl' op (f br) (map (parBranchFoldMap (n - 1) f op) ts `using` myParList rdeepseq)
-  | otherwise = error "parBranchFoldMap: n is negative."
+  | otherwise = branchFoldMap f op t
 
 nodeFoldMap :: (a -> b) -> (b -> b -> b) -> Tree e a -> b
 nodeFoldMap f op (Node _ lb ts) = foldl' op (f lb) $ map (nodeFoldMap f op) ts
 
 -- | Map and fold over nodes. Evaluate the sub trees up to given layer in parallel.
 parNodeFoldMap :: NFData b => Int -> (a -> b) -> (b -> b -> b) -> Tree e a -> b
-parNodeFoldMap 0 f op t = nodeFoldMap f op t
-parNodeFoldMap n f op (Node _ lb ts)
+parNodeFoldMap n f op t@(Node _ lb ts)
   | n >= 1 = foldl' op (f lb) (map (parNodeFoldMap (n - 1) f op) ts `using` myParList rdeepseq)
-  | otherwise = error "parNodeFoldMap: n is negative."
+  | otherwise = nodeFoldMap f op t
