300b971e9a041c261ae2d0a03ce3c9556a2d47c7
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-08-26 15:55:38 +0200

    use cabal

--- flake.nix
+++ flake.nix
@@ -1,28 +1,56 @@
 {
-  description = "Development environment for ELynx.";
+  description = "ELynx";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (
       system:
         let
-          pkgs = import nixpkgs { inherit system; };
+          lib = nixpkgs.lib;
+          packageNames = [
+            "elynx"
+            "elynx-markov"
+            "elynx-nexus"
+            "elynx-seq"
+            "elynx-tools"
+            "elynx-tree"
+            "slynx"
+            "tlynx"
+          ];
+          elynx-create-package = f: name: f name (./. + "/${name}") rec {};
+          elynx-overlay = (
+            selfn: supern: {
+              haskellPackages = supern.haskellPackages.override {
+                overrides = selfh: superh:
+                  lib.genAttrs packageNames
+                    (elynx-create-package selfh.callCabal2nix);
+              };
+            }
+          );
+          overlays = [ elynx-overlay ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          elynx = lib.genAttrs packageNames (
+            # This should be removed but it does not work (see below).
+            n: pkgs.haskell.lib.doBenchmark pkgs.haskellPackages.${n}
+          );
         in
           {
-            devShell = pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [ cabal-install ghc ];
-              buildInputs = with pkgs; [
-                blas
-                gfortran
-                glpk
-                gsl
-                lapack
-                pkg-config
-                sundials
-                zlib
+            packages = elynx;
+
+            defaultPackage = elynx.elynx;
+
+            devShell = pkgs.haskellPackages.shellFor {
+              packages = p: map (n: p.${n}) packageNames;
+              buildInputs = with pkgs.haskellPackages; [
+                haskell-language-server
+                cabal-install
               ];
+              # TODO.
+              # Somehow this flag does not work.
+              doBenchmark = true;
             };
           }
     );
