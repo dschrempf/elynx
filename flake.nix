a931a9fa36800fc9dd713b1973cb23d9b907e0b5
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-09-03 21:41:26 +0200

    fix scripts, use bashInteractive

--- flake.nix
+++ flake.nix
@@ -5,51 +5,56 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          lib = nixpkgs.lib;
-          packageNames = [
-            "elynx"
-            "elynx-markov"
-            "elynx-nexus"
-            "elynx-seq"
-            "elynx-tools"
-            "elynx-tree"
-            "slynx"
-            "tlynx"
-          ];
-          elynx-create-package = f: name: f name (./. + "/${name}") rec {};
-          elynx-overlay = (
-            selfn: supern: {
-              haskellPackages = supern.haskellPackages.override {
-                overrides = selfh: superh:
-                  lib.genAttrs packageNames
-                    (elynx-create-package selfh.callCabal2nix);
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    }:
+      flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            lib = nixpkgs.lib;
+            packageNames = [
+              "elynx"
+              "elynx-markov"
+              "elynx-nexus"
+              "elynx-seq"
+              "elynx-tools"
+              "elynx-tree"
+              "slynx"
+              "tlynx"
+            ];
+            elynx-create-package = f: name: f name (./. + "/${name}") rec {};
+            elynx-overlay = (
+              selfn: supern: {
+                haskellPackages = supern.haskellPackages.override {
+                  overrides = selfh: superh:
+                    lib.genAttrs packageNames
+                      (elynx-create-package selfh.callCabal2nix);
+                };
+              }
+            );
+            overlays = [ elynx-overlay ];
+            pkgs = import nixpkgs { inherit system overlays; };
+            # Set with packages.
+            elynx = lib.genAttrs packageNames (n: pkgs.haskellPackages.${n});
+            # List with packages with benchmark dependencies for development
+            # environment.
+            elynx-dev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) elynx;
+          in
+            {
+              packages = elynx;
+
+              devShell = pkgs.haskellPackages.shellFor {
+                packages = _: (builtins.attrValues elynx-dev);
+                buildInputs = with pkgs; [
+                  bashInteractive
+                  haskellPackages.cabal-install
+                  haskellPackages.haskell-language-server
+                  haskellPackages.stack
+                ];
+                doBenchmark = true;
               };
             }
-          );
-          overlays = [ elynx-overlay ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          # Set with packages.
-          elynx = lib.genAttrs packageNames (n: pkgs.haskellPackages.${n});
-          # List with packages with benchmark dependencies for development
-          # environment.
-          elynx-dev = builtins.mapAttrs (_: x: pkgs.haskell.lib.doBenchmark x) elynx;
-        in
-          {
-            packages = elynx;
-
-            devShell = pkgs.haskellPackages.shellFor {
-              packages = _: (builtins.attrValues elynx-dev);
-              buildInputs = with pkgs; [
-                haskellPackages.cabal-install
-                haskellPackages.haskell-language-server
-                haskellPackages.stack
-              ];
-              doBenchmark = true;
-            };
-          }
-    );
+      );
 }
