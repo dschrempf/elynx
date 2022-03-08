7d94e1e6578faf2a55e822b249eae89c15108d67
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-03-08 06:15:45 +0100

    nix flake update: .

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/haskell-updates";
+  # inputs.nixpkgs.url = "path:/home/dominik/Nix/Nixpkgs";
 
   outputs =
     { self
